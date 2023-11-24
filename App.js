4ab91e192557abce10aabd49a17faf991aade9d5
Gabriel Arnez <gabrielarnez@Gabriels-MacBook-Air.local>
2023-11-24 00:16:05 -0300

    fix: load fonts

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import { StatusBar, SafeAreaView, View } from 'react-native';
+import { useCallback, useEffect } from "react";
+import { StatusBar, SafeAreaView, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
 
-import Cesta from './src/telas/Cesta';
+import Cesta from "./src/telas/Cesta";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Montserrat-Black": require("./assets/fonts/Montserrat-Black.ttf"),
+    "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+    "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-Semi-Bold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaView>
+    <SafeAreaView onLayout={onLayoutRootView}>
       <StatusBar />
       <Cesta />
     </SafeAreaView>
   );
 }
-
--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,87 +1,93 @@
 import React from "react";
-import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  Dimensions,
+  Text,
+  View,
+  SafeAreaView,
+} from "react-native";
 
-import topo from '../../assets/topo.png'
-import logo from '../../assets/logo.png'
+import topo from "../../assets/topo.png";
+import logo from "../../assets/logo.png";
 
-const width = Dimensions.get('screen').width;
+const width = Dimensions.get("screen").width;
 
-export default function Cesta () {
-    return <>
-    <Image source={topo} style={estilos.topo} />
-    <Text style={estilos.titulo}>Detalhe da Cesta</Text>
+export default function Cesta() {
+  return (
+    <SafeAreaView>
+      <Image source={topo} style={estilos.topo} />
+      <Text style={estilos.titulo}>Detalhe da Cesta</Text>
 
-    <View style={estilos.cesta}>
+      <View style={estilos.cesta}>
         <Text style={estilos.nome}>Cesta de Verduras</Text>
         <View style={estilos.fazenda}>
-            <Image source={logo} style={estilos.logoFazenda} />
-        <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+          <Image source={logo} style={estilos.logoFazenda} />
+          <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
         </View>
         <Text style={estilos.descricao}>
-            Uma cesta com produtos selecionados
-            cuidadosamente da fazenda direto 
-            para sua cozinha
+          Uma cesta com produtos selecionados cuidadosamente da fazenda direto
+          para sua cozinha
         </Text>
         <Text style={estilos.preco}>R$ 40,00</Text>
-    </View>
-    </>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const estilos = StyleSheet.create({
-    topo: { width: "100%",
-    height: 578 / 768 * width,
-    },
+  topo: { width: "100%", height: (578 / 768) * width },
 
-    titulo: {
-        width: '100%',
-        position: 'absolute',
-        textAlign: 'center',
-        fontSize: 16,
-        lineHeight: 26,
-        color: 'white',
-        fontWeight: 'bold',
-        padding: 16,
-    },
+  titulo: {
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    fontSize: 16,
+    lineHeight: 26,
+    color: "white",
+    padding: 16,
+    fontFamily: "Montserrat-Medium",
+  },
 
-    cesta: {
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-    },
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
 
-    nome: {
-        fontSize: 26,
-        lineHeight: 42,
-        color:"#464646",
-        fontWeight: "bold",
-    },
+  nome: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: "#464646",
+    fontFamily: "Montserrat-Semi-Bold",
+  },
 
-    fazenda: {
-        flexDirection: 'row',
-        paddingVertical: 12,
-    }, 
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
 
-    logoFazenda: {
-        width:32,
-        height: 32,
-    },
+  logoFazenda: {
+    width: 32,
+    height: 32,
+  },
 
-    nomeFazenda: {
-        fontSize: 16,
-        lineHeight: 26,
-        marginLeft: 12,
-    },
+  nomeFazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+  },
 
-    descricao: {
-        color: "#A3A3A3",
-        fontSize: 16,
-        lineHeight: 26,
-    },
+  descricao: {
+    color: "#A3A3A3",
+    fontSize: 16,
+    lineHeight: 26,
+  },
 
-    preco: {
-        color: "#2A9F85",
-        fontSize: 24,
-        fontWeight: 'bold',
-        lineHeight: 42,
-        marginTop: 8,
-    }
-}); 
+  preco: {
+    color: "#2A9F85",
+    fontSize: 24,
+    fontWeight: "bold",
+    lineHeight: 42,
+    marginTop: 8,
+  },
+});
